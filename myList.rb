76f2495aa440c8c02a3e98445eda34bcf4a56e64
Waweru <ngashalex8@gmail.com>
2023-07-27 17:47:11 +0300

    Enumerables project

--- myList.rb
+++ myList.rb
@@ -1,9 +1,34 @@
 require_relative 'myEnumerable'
 
 class MyList
-    include MyEnumerable
+  include MyEnumerable
 
-    def each(&block)
-        @list.each(&block)
-      end
-    end
+  def initialize(*args)
+    @list = args
+  end
+
+  def each(&block)
+    @list.each { |item| block.call(item) }
+  end
+end
+
+
+# class MyList
+#     include MyEnumerable
+
+#     def initialize(*data)
+#       @list = data
+#     end
+
+#     def each(&block)
+#         @list.each(&block)
+#       end
+#     end
+
+    # class MyList
+    #   include MyEnumerable
+    
+    #   def initialize(data)
+    #     @list = data
+    #   end
+    # end
--- test.rb
+++ test.rb
@@ -1,8 +1,11 @@
 require_relative 'mylist'
 
+# Verify your solution:
+
+# Create our list
 list = MyList.new(1, 2, 3, 4)
 puts (list.all? { |e| e < 5 })
 puts (list.all? { |e| e > 5 })
 puts (list.any? { |e| e == 2 })
 puts (list.any? { |e| e == 5 })
-puts (list.filter(&:even?))
+p list.filter {|e| e.even?}
